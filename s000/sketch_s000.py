1231d385251b53f126850ebe0569836535a682d5
Daniele D'Orazio <d.dorazio96@gmail.com>
2022-02-28 22:37:05 +0100

    s000: improvements

--- s000/sketch_s000.py
+++ s000/sketch_s000.py
@@ -1,3 +1,4 @@
+from decimal import DivisionByZero
 from locale import normalize
 import vsketch
 import random
@@ -31,8 +32,8 @@ class S18022022Sketch(vsketch.SketchClass):
         quads = []
 
         while yl < 26 and yr < 26:
-            dyl = random.randrange(2, 6)
-            dyr = dyl + random.randrange(1, 2)
+            dyl = 3.5
+            dyr = dyl + random.randrange(1, 3)
 
             q = [
                 (x0, yl),
@@ -43,42 +44,83 @@ class S18022022Sketch(vsketch.SketchClass):
             quads.append(q)
             yl, yr = yl + dyl, yr + dyr
 
-            s = 1 if len(quads) % 2 == 0 else -1
-            s *= vsk.random(0.3, 1.3)
+            s = random.choice([1, 2]) * (len(quads) % 2 * 2 - 1)
 
-            x0, yl = lerp_seg(q[1], q[2], -s)
-            if self.flip:
-                x1, yr = lerp_seg(q[2], q[1], -s)
-            else:
-                x1, yr = lerp_seg(q[2], q[1], s)
+            try:
+                x0, yl = lerp_seg(q[1], q[2], -s)
+                if self.flip:
+                    x1, yr = lerp_seg(q[2], q[1], -s)
+                else:
+                    x1, yr = lerp_seg(q[2], q[1], s)
+            except ZeroDivisionError:
+                break
+
+        if yl < 24:
+            x1, yr = lerp_seg(
+                quads[-1][1], quads[-1][2], vsk.random(0.75, 0.95), normalize=False
+            )
+            quads.append(
+                [
+                    (x0, yl),
+                    (x0, yr),
+                    (x1, yr),
+                    (x1, yr),
+                ]
+            )
+
+        vsk.line(-2, 0, 16, 0)
 
         for i, q in enumerate(quads):
             vsk.polygon(q, close=True)
 
             xl, y0 = q[0]
             _, y1 = q[1]
+            xr, _ = q[2]
 
             lines = []
-            for y in np.arange(y0, y1, 0.05):
-                lines.append(LineString([(xl, y), (xl + vsk.random(0.3, 0.8), y)]))
+            for y in np.arange(y0, y1, 0.1):
+                lines.append(LineString([(xl, y), (xl + vsk.random(0.5, 1.2), y)]))
+
+            for x in np.arange(xl, xl + 0.4, 0.1):
+                lines.append(
+                    LineString(
+                        [
+                            (x, q[2][1]),
+                            (x, vsk.lerp(q[2][1], y0, 1.0 - (x - xl) / 0.4 / 10.0)),
+                        ]
+                    )
+                )
+
+            for x in np.arange(xr - 0.9, xr, 0.2):
+                print((x - xr + 0.9) / 0.9)
+                lines.append(
+                    LineString(
+                        [
+                            (x, q[2][1]),
+                            (
+                                x,
+                                vsk.lerp(q[2][1], y0, (x - xr + 0.9) / 0.9),
+                            ),
+                        ]
+                    )
+                )
+
+            for y in np.arange(y1, y1 + 0.5, 0.1):
+                l = LineString([(xl, y), (xl + 3, y)]) & Polygon(q)
+                if len(l.coords) < 2:
+                    continue
+                p0, _ = l.coords
+                lines.append(
+                    LineString([(p0[0], y), (p0[0] + vsk.random(0.3, 0.8), y)])
+                )
 
             vsk.geometry(MultiLineString(lines) & Polygon(q))
 
-            for y in np.arange(y1, y1 + 0.5, 0.05):
-                l = LineString([(xl, y), (xl + 3, y)]) & Polygon(q)
-                if l.is_empty:
-                    continue
-                p0, _ = l.coords
-                vsk.line(p0[0], y, p0[0] + vsk.random(0.3, 0.8), y)
-
-            vsk.strokeWeight(5)
             t = 1 - i / len(quads)
             t = vsk.random(max(0.1, t - 1 / len(quads)), t)
 
             vsk.line(*q[0], *lerp_seg(q[0], q[3], t, normalize=False))
             vsk.line(*q[1], *lerp_seg(q[1], q[2], t, normalize=False))
-
-            vsk.strokeWeight(1)
 
     def finalize(self, vsk: vsketch.Vsketch) -> None:
         vsk.vpype("linemerge linesimplify reloop linesort")
